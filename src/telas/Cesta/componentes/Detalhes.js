e5992622e00291f408d2b4b14896e2e9d4669701
Giovana <giovanaworkfg@gmail>
2022-12-28 21:27:21 -0300

    incluiu pasta de estilos -detalhes

--- src/telas/Cesta/componentes/Detalhes.js
+++ src/telas/Cesta/componentes/Detalhes.js
@@ -1,12 +1,15 @@
 /*IMPORT PARA CONSEGUIR RODAR AS FUNÇÕES*/
 import React from 'react';
-import { View, Image, StyleSheet, Text} from 'react-native';
+import { View, Image, Text} from 'react-native';
 
-/*FUNÇÃO - PARA CONSEGUIR PUXAR O TEXTO/IMG DO ARQUIVO MOCKS/CESTA, FOI NECESSÁRIO CHAMAR DENTRO DA FUNÇÃO OS PARÂMETROS*/
-export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, preco }){
+import estilos from './StylesDetalhes.js';
+
+
+/*FUNÇÃO*/
+export default function Detalhes({nome, logoFazenda, nomeFazenda, descricao, preco }){
     return <>
           {/*DESCRIÇÃO DO PRODUTO*/}
-            <Text style={estilos.nome}>{nome}</Text>
+            <Text style={estilos.nome} >{nome}</Text>
 
             {/*VIEW PARA ARMAZENAR COTEÚDO COM NOME DA FAZENDA E ICONE*/}
             <View style={estilos.fazenda}>
@@ -17,51 +20,4 @@ export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, pr
             <Text style={estilos.descricao}>{descricao}</Text>
             <Text style={estilos.preco}>{preco}</Text>    
     </>
-}
-
-/*ESTILO*/
-const estilos = StyleSheet.create({
-    /*NOME*/
-    nome: {
-        lineHeight: 42,
-        fontSize: 26,
-        color: "#464646",
-        fontWeight: "bold",
-    },
-
-    /*VIEW FAZENDA*/
-    fazenda: {
-        /*NÃO É PRECISO DEFINIR DISPLAY FLEX POIS NO REACT NATIVE JÁ VEM POR PADRÃO*/
-        flexDirection: "row",
-        paddingVertical: 12,
-    },
-
-    /*IMAGEM FAZENDA*/
-    imagemFazenda: {
-        width: 32,
-        height: 32
-    },
-
-    /*NOME FAZENDA*/
-    nomeFazenda: {
-        fontSize: 16,
-        lineHeight: 26,
-        marginLeft: 12,
-    },
-
-    /*DESCRIÇÃO*/
-    descricao: {
-        color: "#A3A3A3",
-        fontSize: 16,
-        lineHeight: 26,
-    },
-
-    /*PREÇO*/
-    preco: {
-        color: "#2A9F85",
-        fontWeight: "bold",
-        fontSize: 26,
-        lineHeight: 42,
-        marginTop: 8,
-    },
-});
+};
---
+++ src/telas/Cesta/componentes/StylesDetalhes.js
@@ -0,0 +1,50 @@
+import { StyleSheet } from 'react-native';
+
+/*ESTILO*/
+const estilos = StyleSheet.create({
+    /*NOME*/
+    nome: {
+        lineHeight: 42,
+        fontSize: 26,
+        color: "#464646",
+        fontWeight: "bold",
+    },
+
+    /*VIEW FAZENDA*/
+    fazenda: {
+        /*NÃO É PRECISO DEFINIR DISPLAY FLEX POIS NO REACT NATIVE JÁ VEM POR PADRÃO*/
+        flexDirection: "row",
+        paddingVertical: 12,
+    },
+
+    /*IMAGEM FAZENDA*/
+    imagemFazenda: {
+        width: 32,
+        height: 32
+    },
+
+    /*NOME FAZENDA*/
+    nomeFazenda: {
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 12,
+    },
+
+    /*DESCRIÇÃO*/
+    descricao: {
+        color: "#A3A3A3",
+        fontSize: 16,
+        lineHeight: 26,
+    },
+
+    /*PREÇO*/
+    preco: {
+        color: "#2A9F85",
+        fontWeight: "bold",
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8,
+    },
+});
+
+export default estilos;
